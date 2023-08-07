dc9ad9e6d0e002a65b4ad1d415a7b34f50d790df
EdsonSalasV <edson.salasvega@outlook.es>
2023-08-06 23:51:58 -0400

    se agrega pagina de detalles

--- src/Pages/Detalles.jsx
+++ src/Pages/Detalles.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { MyContext } from '../context/MyContext'
 
-const Detalles = ({funkos}) => {
+const Detalles = () => {
+
+  const { dataFunkos } = useContext(MyContext)
+
+  const { id } = useParams()
+
+  const funkoDetalle = dataFunkos.find(item => item.id == id)
+  console.log(funkoDetalle)
+
+
   return (
-    <div>
-      <h1>detalles</h1>
-    </div>
+    <main className="m-2">
+      <div className='d-flex flex-row'>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={funkoDetalle.image} className="card-img-top" alt="Descripción de la imagen" />
+          <div className="card-body">
+            <h5 className="card-title">{funkoDetalle.nombre}</h5>
+            <p className="card-text">{funkoDetalle.detalles}</p>
+            
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
